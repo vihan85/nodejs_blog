2164add3dc00becf22910a86e21e9a27ba48304c
vihan85 <hanthuyvi0805@gmail.com>
2023-02-24 16:02:16 +0700

    themplate-engine

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path')
 const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
+// config get static file
+app.use(express.static(path.join(__dirname, 'public')))
 // HTTP logger
 app.use(morgan('combined'));
 //template engine
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+app.engine('.hbs', handlebars.engine({
+  extname: ".hbs"
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
   res.render('home');
+});
+app.get('/news', (req, res) => {
+  res.render('news');
 });
 
 app.listen(port, () => {
