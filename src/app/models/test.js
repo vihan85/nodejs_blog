d90889cdbdc95fb68334d22a8b9abea48803dd93
vihan85 <hanthuyvi0805@gmail.com>
2023-03-07 13:58:21 +0700

    mongoose-slug

--- src/app/models/test.js
+++ src/app/models/test.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 const Schema = mongoose.Schema
 const Data = new Schema ({
     title: String,
     body: String,
-    slug: String,
     des: String,
-    thum: String
+    thum: String,
+    slug: { type: String, slug: "title"}
 })
 module.exports = mongoose.model('Data', Data)
