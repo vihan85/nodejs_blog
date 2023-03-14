08f91066b0058baddeacbb7b6f85a9a8c3fedf6a
vihan85 <hanthuyvi0805@gmail.com>
2023-03-14 16:19:18 +0700

    midleware

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://node-blog:yMzStk9a3J7GWQQD@cluster0.7pwrkrt.mongodb.net/data?retryWrites=true&w=majority";
+const uri = "mongodb+srv://node-blog:m0np6huXaxB5UUOc@cluster0.7pwrkrt.mongodb.net/data?retryWrites=true&w=majority";
 async function connect () {
     try {
         await mongoose.connect(uri, {
